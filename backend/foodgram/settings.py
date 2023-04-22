917a3ad3f7b700b02297bf999c6bba762d2ae292
Oskalovlev <111913579+Oskalovlev@users.noreply.github.com>
2023-04-22 22:38:09 +0300

    v3.8

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -141,11 +141,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/b-static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'b-static')
 
-MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/b-media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'b-media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
