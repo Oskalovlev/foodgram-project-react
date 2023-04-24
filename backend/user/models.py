cfb7724e7e60ad4df39b8d0160e98c7d22d45c63
Oskalovlev <111913579+Oskalovlev@users.noreply.github.com>
2023-04-24 10:11:12 +0300

    v4.1

--- backend/user/models.py
+++ backend/user/models.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from django.contrib.auth.models import AbstractUser
-from django.core.validators import MinValueValidator, RegexValidator
+from django.core.validators import RegexValidator
 from django.db import models
 
 from .validators import username_validator
